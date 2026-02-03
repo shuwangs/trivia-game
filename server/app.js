db0a63600664958ce967b3d4d36d37cc0be806c1
Shu <shuw425@gmail.com>
2026-02-02 23:12:43 -0500

    refactor(server): split app and server entry point for easier testing -  Moved Express app setup to app.js - Isolated app.listen() in index.js

---
+++ server/app.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+dotenv.config();
+
+const BASE_URL = process.env.OPEN_TRIVIA_BASE_URL;
+console.log(BASE_URL);
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/api/game', async (req, res) =>{
+    try {
+        const params = new URLSearchParams()
+
+        const { amount, category, difficulty, type } = req.query;
+        if (amount) params.append("amount", amount);
+        if (category) params.append("category", category);
+        if (difficulty) params.append("difficulty", difficulty);
+        if (type) params.append("type", type);
+
+        const url=`${BASE_URL}?${params.toString()}`;
+        console.log(url);
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        console.log(data);
+        
+        // return data
+        return res.json(data);
+        
+    } catch (err) {
+        console.error(err);
+    }
+
+})
+
+export default app;
--- server/index.js
+++ server/index.js
@@ -1,44 +1,7 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-dotenv.config();
-
-
-const PORT = process.env.PORT || 3000;
-const BASE_URL = process.env.OPEN_TRIVIA_BASE_URL;
-console.log(BASE_URL);
-const app = express();
-
-// middleware
-app.use(cors());
-app.use(express.json());
-
-
-
-app.get('/api/game', async (req, res) =>{
-    try {
-        const params = new URLSearchParams()
-
-        const { amount, category, difficulty, type } = req.query;
-        if (amount) params.append("amount", amount);
-        if (category) params.append("category", category);
-        if (difficulty) params.append("difficulty", difficulty);
-        if (type) params.append("type", type);
-
-        const url=`${BASE_URL}?${params.toString()}`;
-        console.log(url);
-
-        const response = await fetch(url);
-        const data = await response.json();
-
-        console.log(data);
-        
-    } catch (err) {
-        console.error(err);
-    }
-
-})
+import app from "./app.js";
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, ()=>{
     console.log(`Listening to port: ${PORT}`);
-})
+});
+
